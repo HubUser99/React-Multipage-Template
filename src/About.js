2692db9ea4103c49f7d38f34eb24e4e6c714a56b
HubUser99 <cliff.goga@gmail.com>
2018-12-30 02:03:29 +0200

    Added dummy info to the web pages

--- src/About.js
+++ src/About.js
@@ -4,7 +4,19 @@ class About extends Component {
   render() {
     return(
       <div className="About">
-        <p>This is about</p>
+        <header>
+          <h1>About</h1>
+        </header>
+        <div className="About-content">
+          <h3>Purpose</h3>
+          <p>This is a multipage web application template.</p>
+
+          <h3>Author</h3>
+          <p>Egor Filonov</p>
+
+          <h3>Date of creation</h3>
+          <p>30.12.2018</p>
+        </div>
       </div>
     );
   }
